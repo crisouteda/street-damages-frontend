823773d2d1967e886427606e0320020c2b58067b
cristina <cristina.outeda.rua@gmail.com>
2021-10-12 12:11:34 +0200

    front working with the backend

--- src/helpers/backendCalls.js
+++ src/helpers/backendCalls.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 export const submitDamage = async (damage, geolocation) => {
-    await axios.post("http://localhost:4000/api/insert", {
-      damage,
-      geolocation,
-      //   user_id: user[0].id,
-    });
-    // await dispatch({ type: "increment" });
-  };
+  await axios.post("http://localhost:4000/insert", {
+    damage,
+    geolocation,
+  });
+};
+
+export const getDamages = async () => {
+  const response = await axios.get("http://localhost:4000/read");
+  return response.data.Items;
+};
