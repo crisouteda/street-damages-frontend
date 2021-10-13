bf1945389f8302e0746d96a351dde3082db11aa5
cristina <cristina.outeda.rua@gmail.com>
2021-10-14 00:07:42 +0200

    splited App layout in two columns + syled

---
+++ src/App.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledApp = styled.div`
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  grid-template-columns: 50% 50%;
+  background: linear-gradient(
+    45deg,
+    rgba(45, 45, 45, 1) 9%,
+    rgba(0, 0, 0, 1) 100%
+  );
+`;
