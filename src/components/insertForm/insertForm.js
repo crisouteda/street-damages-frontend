5394a4168940ddcd603ec4a3a5f90df16da81e73
cristina <cristina.outeda.rua@gmail.com>
2021-10-14 00:12:01 +0200

    imported style files + added structure

--- src/components/insertForm/insertForm.js
+++ src/components/insertForm/insertForm.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { getDamages, getGeolocation, submitDamage } from "../../helpers";
+import { StyledForm } from "./insertForm.style";
+import "./insertForm.css";
 
 export function InsertForm() {
   const [damage, setDamage] = useState("");
   const [geolocation, setGeolocation] = useState({});
   const [storedDamages, setStoredDamages] = useState([]);
   const [province, setProvince] = useState("");
+  console.log(storedDamages);
 
   useEffect(() => {
     const _geolocation = getGeolocation();
@@ -25,30 +28,34 @@ export function InsertForm() {
   }, []);
 
   return (
-    <div style={{ background: "red" }}>
-      <h1>Insert a damage</h1>
-      <select
-        id="damage"
-        name="damage"
-        onChange={(e) => setDamage(e.target.value)}
-      >
-        {damageOptions.map((damageOption) => (
-          <option value={damageOption.value}>{damageOption.text}</option>
-        ))}
-      </select>
-      <span>Write your province</span>
-      <input
-        onChange={(e) => {
-          setProvince(e.target.value);
-        }}
-      ></input>
-      <button
-        onClick={() => {
-          submitDamage(damage, geolocation, province);
-        }}
-      >
-        Submit
-      </button>
-    </div>
+    <StyledForm>
+      <h1 data-heading="t">Insert a damage</h1>
+      <div>
+        <select
+          id="damage"
+          name="damage"
+          onChange={(e) => setDamage(e.target.value)}
+        >
+          {damageOptions.map((damageOption) => (
+            <option value={damageOption.value}>{damageOption.text}</option>
+          ))}
+        </select>
+        <div>
+          <span>Write your province</span>
+          <input
+            onChange={(e) => {
+              setProvince(e.target.value);
+            }}
+          ></input>
+        </div>
+        <button
+          onClick={() => {
+            submitDamage(damage, geolocation, province);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </StyledForm>
   );
 }
